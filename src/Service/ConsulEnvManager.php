7a412dc8a5b8818cd8bfdbc75743910cad66025b
Petre Pătrașc <petre@dreamlabs.ro>
2017-02-01 19:27:17 +0200

    Add Consul environment manager

---
+++ src/Service/ConsulEnvManager.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace DL\ConsulPhpEnvVar\Service;
+
+use SensioLabs\Consul\Services\KV;
+
+/**
+ * Manages environment variables through Consul.
+ *
+ * @package DL\ConsulPhpEnvVar\Service
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class ConsulEnvManager
+{
+    /**
+     * @var KV
+     */
+    protected $kv;
+
+    /**
+     * @var bool
+     */
+    protected $overwriteEvenIfDefined = false;
+
+    /**
+     * ConsulEnvManager constructor.
+     *
+     * @param KV   $kv
+     * @param bool $overwriteEvenIfDefined
+     */
+    public function __construct(KV $kv, bool $overwriteEvenIfDefined = false)
+    {
+        $this->kv                     = $kv;
+        $this->overwriteEvenIfDefined = $overwriteEvenIfDefined;
+    }
+
+    /**
+     * Get environment variables from Consul.
+     *
+     * @param array $mappings
+     */
+    public function getEnvVarsFromConsul(array $mappings): void
+    {
+        foreach ($mappings as $envKey => $kvPath) {
+            $keyExists = (false === getenv($envKey)) ? false : true;
+            if ($keyExists && !$this->overwriteEvenIfDefined) {
+                continue;
+            }
+
+            $kvValue = $this->kv->get($kvPath)->getBody();
+            putenv("{$envKey}={$kvValue}");
+        }
+    }
+}
---
+++ tests/Service/ConsulEnvManagerTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types = 1);
+
+namespace DL\ConsulPhpEnvVar\Tests\Service;
+
+use DL\ConsulPhpEnvVar\Service\ConsulEnvManager;
+use PHPUnit\Framework\TestCase;
+use SensioLabs\Consul\ConsulResponse;
+use SensioLabs\Consul\Services\KV;
+
+/**
+ * Test the behaviour of the Consul environment manager.
+ *
+ * @package DL\ConsulPhpEnvVar\Tests\Service
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class ConsulEnvManagerTest extends TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $kv;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->kv = $this->getMockBuilder(KV::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testGivenThatAnEnvironmentVariableIsNotDefinedThenItsValueWillBeRetrievedFromConsul()
+    {
+        $testKey = 'TEST_ENV_1';
+        $response = new ConsulResponse([], 'test_value');
+        $this->kv->expects($this->once())
+            ->method('get')
+            ->with('test/env')
+            ->willReturn($response);
+
+        $this->assertFalse(getenv($testKey));
+
+        $manager = new ConsulEnvManager($this->kv);
+        $manager->getEnvVarsFromConsul([
+            $testKey => 'test/env',
+        ]);
+
+        $this->assertEquals('test_value', getenv($testKey));
+    }
+
+    public function testGivenThatAnEnvironmentVariableIsDefinedThenItsValueWillNotBeRetrievedFromConsul()
+    {
+        $testKey = 'TEST_ENV_2';
+        $this->kv->expects($this->never())
+            ->method('get');
+
+        putenv("{$testKey}=already-defined");
+
+        $manager = new ConsulEnvManager($this->kv);
+        $manager->getEnvVarsFromConsul([
+            $testKey => 'test/env',
+        ]);
+
+        $this->assertEquals('already-defined', getenv($testKey));
+    }
+
+    public function testGivenThatAnEnvironmentVariableIsDefinedThenItsValueWillStillBeRetrievedFromConsulIfTheOverwriteOptionIsDefined()
+    {
+        $testKey = 'TEST_ENV_3';
+        $response = new ConsulResponse([], 'test_value_3');
+        $this->kv->expects($this->once())
+            ->method('get')
+            ->with('test/env3')
+            ->willReturn($response);
+
+        $this->assertFalse(getenv($testKey));
+        putenv("{$testKey}=already-defined");
+
+        $manager = new ConsulEnvManager($this->kv, true);
+        $manager->getEnvVarsFromConsul([
+            $testKey => 'test/env3',
+        ]);
+
+        $this->assertEquals('test_value_3', getenv($testKey));
+    }
+}
