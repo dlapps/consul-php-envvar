689091d5fd14df3ba1b48a356ded2c4611df5d06
Petre Pătrașc <petre@dreamlabs.ro>
2017-02-02 11:43:28 +0200

    Refactor the method that retrieves missing env keys from Consul

--- src/Service/ConsulEnvManager.php
+++ src/Service/ConsulEnvManager.php
@@ -36,20 +36,61 @@ class ConsulEnvManager
     }
 
     /**
-     * Get environment variables from Consul.
+     * Add missing environment variables from Consul.
      *
      * @param array $mappings
      */
     public function getEnvVarsFromConsul(array $mappings)
     {
-        foreach ($mappings as $envKey => $kvPath) {
-            $keyExists = (false === getenv($envKey)) ? false : true;
+        foreach ($mappings as $environmentKey => $kvPath) {
+            $keyExists = $this->keyIsDefined($environmentKey);
             if ($keyExists && !$this->overwriteEvenIfDefined) {
                 continue;
             }
 
-            $kvValue = $this->kv->get($kvPath)->getBody();
-            putenv("{$envKey}={$kvValue}");
+            $consulValue = $this->getKeyValueFromConsul($kvPath);
+            $this->saveKeyValueInEnvironmentVars($environmentKey, $consulValue);
         }
+    }
+
+    /**
+     * Check if an environment key is defined.
+     *
+     * @param string $environmentKey
+     *
+     * @return bool
+     */
+    private function keyIsDefined(string $environmentKey): bool
+    {
+        $keyValue = getenv($environmentKey);
+
+        if (false === $keyValue) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get a key value from Consul.
+     *
+     * @param string $kvPath
+     *
+     * @return string
+     */
+    private function getKeyValueFromConsul(string $kvPath): string
+    {
+        return $this->kv->get($kvPath)->getBody();
+    }
+
+    /**
+     * Save the value of a key in an environment variable.
+     *
+     * @param string $envKey
+     * @param string $kvValue
+     */
+    private function saveKeyValueInEnvironmentVars($envKey, $kvValue)
+    {
+        putenv("{$envKey}={$kvValue}");
     }
 }
